b1720bf449f605bc2cf55ebccc6e6bd96857d3e4
Dustin Aguilar <dustin@dustinaguilar.com>
2015-04-29 11:29:30 -0400

    First Upload - Maintenance
    Updated year to 2015 and returned contact form to it's previous setting,
    which is just a basic  form as opposed to an iframe with something else
    in it.

---
+++ free_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "asamedayapplianceservice@gmail.com"; // your email address
+$email_subject = "Repair Inquiry from Website"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+?>
