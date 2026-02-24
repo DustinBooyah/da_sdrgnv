4424358c7067341f5cef27c557861312d7d30b5e
daguilar <dustin@dustinaguilar.com>
2026-02-24 03:10:54 -0500

    ADA Compliance
    Optimize site, and ensure ADA compliance.

--- form_settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$email_to = "asamedayapplianceservice@gmail.com"; // your email address
-$email_subject = "Repair Inquiry from Website"; // email subject line
-$thankyou = "thankyou.htm"; // thank you page
-
-?>
